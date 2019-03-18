c53dfc9f64efcd6dfebf554c20960ec16fe45e4a
MCSens <MCSens@web.de>
2019-03-18 07:04:04 +0100

    Logback Test, unstaged changes

---
+++ SubtitlesToCSV/src/svp/test/SubtitleDataholderAsTableTest.java
@@ -0,0 +1,31 @@
+package svp.test;
+
+import svp.data.filereader.AudacityFileReader;
+import svp.data.main.SubtitleDataholder;
+import svp.data.main.SubtitleTableModel;
+import svp.gui.main.TestCardLayout;
+
+public class SubtitleDataholderAsTableTest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String movieName = "NPCR2-Text1-Dialog2";
+		String subtitleFilePath = "C:\\Users\\MCSens\\git\\S2Anki\\SubtitlesToCSV\\resources\\SubtitleDataholderAsTableTest.txt";
+		String audioFilePath = "C:\\Users\\MCSens\\Subtitles\\Audio\\02_Text1_Dialog2.mp3";
+		String[] languages = new String[]{"Hanzi","Pinyin","English"};
+		AudacityFileReader afr = new AudacityFileReader(movieName, 
+				subtitleFilePath, 
+				audioFilePath, 
+				languages);
+		afr.readFile();
+		//afr.mergeCommas();
+		
+		SubtitleDataholder sdh = SubtitleDataholder.getSubtitleDataholder();
+		int rows = sdh.getRowCount();
+		int columns = sdh.getColumnCount();
+		for(int i = 0; i<rows;i++) {
+			for(int j = 0; j<columns;j++) {
+				System.out.println(sdh.getValueAt(i, j));
+			}
+		}
+	}
+}
